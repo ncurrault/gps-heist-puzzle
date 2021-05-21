a0acd3d420ec3c34e84965b39be356238344df89
Nicholas Currault <nicholas.curr@gmail.com>
2021-05-20 22:44:18 -0700

    better map display (still need other users)

--- client/map-display.js
+++ client/map-display.js
@@ -4,33 +4,49 @@ const homeRadius = 10;
 const currRadius = 10;
 const mapSize = 0.003614; // approx. quarter mile
 
-function drawCanvas(locData) {
-    canvas.height = canvas.width;
-
+function clear(canvas) {
     var ctx = canvas.getContext("2d");
-
-    // TODO the actual puzzle
     ctx.beginPath();
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "black";
     ctx.fill();
+}
+
+function dot(canvas, x, y, r, col) {
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+    ctx.fillStyle = col;
+    ctx.fill();
+}
+
+/* https://stackoverflow.com/questions/15397036/drawing-dashed-lines-on-html5-canvas */
+function dottedLine(canvas, x1, y1, x2, y2, col) {
+    var ctx = canvas.getContext("2d");
+
+    ctx.setLineDash([5, 3]); /* 5px dashes, 3px spaces */
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = col;
+    ctx.stroke();
+}
+
+function drawCanvas(locData) {
+    canvas.height = canvas.width;
+    clear(canvas);
 
     var midX = canvas.width / 2, midY = canvas.height / 2;
 
-    // home dot
-    ctx.beginPath();
-    ctx.arc(midX, midY, homeRadius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
-    ctx.fill();
+    // home
+    dottedLine(canvas, midX, 0, midX, canvas.height, 'white');
+    dottedLine(canvas, 0, midY, canvas.width, midY, 'white');
 
     // current dot
     var currX = canvas.width * locData.local.x / mapSize + midX;
     var currY = canvas.height * locData.local.y / mapSize + midY;
-
-    ctx.beginPath();
-    ctx.arc(currX, currY, currRadius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'red';
-    ctx.fill();
+    dot(canvas, midX, midY, 5, 'blue');
 
     // TODO draw other users, each labelled with initials
     // TODO draw mean pos as a dot
